e3418e88708f9a7b3a21beff579827e1c02e6b99
Namelessnessness <rainierderryl@gmail.com>
2020-06-10 22:28:15 +0800

    Made changes to GameController.cpp and GameController.h. Some issues found, but will be fixed tomorrow.

--- src/controllers/GameController.cpp
+++ src/controllers/GameController.cpp
@@ -1,8 +1,99 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
 #include "GameController.h"
+#include "Computer.h"
+#include "Player.h"
+#include "Deck.h"
 
-void GameController::betMoney(int money) {
-  if(remainingMoney<money)
-    throw "Insufficient money";
-  else 
-	remainingMoney-=money;			
+
+using namespace std;
+
+GameController::GameController()
+{
+}
+
+int GameController::start(int option)
+{
+	switch(option)
+	{
+		case 1:
+			{
+			return 1;
+			}
+		case 2:
+			{
+			int nextOption=0;
+			cout<<highScore<<endl;
+			cout<<"Next option: ";
+			cin>>nextOption;
+			start(nextOption);
+			break;		
+			}
+		case 3:
+			{
+			return 0;
+			break;
+			}
+		default:
+			{
+			int nextOption=0;
+			cout<<"Invalid option! Please enter an option from 1-3.\n Next option: ";
+			cin>>nextOption;
+			start(nextOption);
+			break;		
+			}
+	}
+}
+
+int GameController::deal()
+{
+	if(remainingCards==0)
+	{
+		return 0;
+	}
+	else if(playerCard_pile==computerCard_pile)
+	{
+		player_Card=*top;
+		++playerCard_pile;
+		--remainingCards;
+	}
+	else
+	{
+		computer_Card=*top;
+		++computerCard_pile;
+		--remainingCards;
+	}
+}
+
+void GameController::shuffle()
+{
+   	for(int i=0;i<2704;i++)
+   	{
+   		int Index1=rand()%52;//chooses a random number from 0 to 51, since the array starts from index 0 to 51. 
+   		int Index2=rand()%52;
+		if( Index1 != Index2 )
+		{
+			Card temp=Deck[Index1];//Making a temporary variable to store the data at Index1 before initializing Index1 to Index2's value.
+			Deck[Index1]=Deck[Index2];
+			Index2=temp;
+			Deck[Index2]=temp;
+		}
+	}
+}
+
+void GameController::wageWinner()
+{
+	if(get_baseValue<computer_Card.get_baseValue)
+	{
+		return 1;
+	}
+	else if(player_Card.get_baseValue>computer_Card.get_baseValue)
+	{
+		return 2;
+	}
+	else
+	{
+		return 3;			
+	}
 }
--- src/controllers/GameController.h
+++ src/controllers/GameController.h
@@ -1,8 +1,15 @@
-class GameController {
-  public: 
-    void betMoney(int money);
-	
-  private:
-    int betMoney;
-	int remainingMoney;
+#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+#endif
+#include "Player.h"
+#include "Computer.h"
+#include "Deck.h"
+class GameController
+{
+	public:
+		GameController();
+		int start(int option);
+		int deal(Player *p, Computer *c,Deck *d);
+		void shuffle(Deck *d);
+		int wageWinner(Player *p, Computer *c);
 };
