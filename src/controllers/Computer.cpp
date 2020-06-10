b2e050e0a412723685003facf44a07d0bef9c184
Namelessnessness <rainierderryl@gmail.com>
2020-06-10 22:27:38 +0800

    Made changes to Computer.cpp and Computer.h. Some issues found, but will be fixed tomorrow.

---
+++ src/controllers/Computer.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include"Computer.h"
+
+Computer::Computer()
+{
+}
---
+++ src/controllers/Computer.h
@@ -0,0 +1,11 @@
+#include "../models/Card.h"
+#ifndef COMPUTER_H
+#define COMPUTER_H
+#endif
+class Computer
+{
+	public:
+		Card computer_Card(0,0);
+		int computerCard_pile=0;
+		Computer();
+};
